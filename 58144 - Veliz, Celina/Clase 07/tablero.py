081316014220543f89c1777b89cb27b930334c7e
Celina Veliz Mercado <you@example.com>
2019-09-26 18:51:40 -0300

    Tateti

--- 58144 - Veliz, Celina/Clase 07/tablero.py
+++ 58144 - Veliz, Celina/Clase 07/tablero.py
@@ -7,21 +7,22 @@ def draw_tablero(tablero):
 def validate (tablero, posicion):
     return tablero[posicion -1]==" "
 def win(tablero):
-    if tablero[0]!=" " and tablero[0]==tablero[4] and tablero[4]==tablero[8]:
+     if tablero[0]!=" " and tablero[0]==tablero[4] and tablero[4]==tablero[8]:
         return True
-    if tablero[2]!=" " and tablero[2]==tablero[4] and tablero[4]==tablero[6]:
+     if tablero[2]!=" " and tablero[2]==tablero[4] and tablero[4]==tablero[6]:
         return True
-    if tablero[0]!=" " and tablero[0]==tablero[3] and tablero[3]==tablero[6]:
+     if tablero[0]!=" " and tablero[0]==tablero[3] and tablero[3]==tablero[6]:
         return True
-    if tablero[1]!=" " and tablero[1]==tablero[4] and tablero[4]==tablero[7]:
+     if tablero[1]!=" " and tablero[1]==tablero[4] and tablero[4]==tablero[7]:
         return True
-    if tablero[2]!=" " and tablero[2]==tablero[5] and tablero[5]==tablero[8]:
+     if tablero[2]!=" " and tablero[2]==tablero[5] and tablero[5]==tablero[8]:
         return True
-    if tablero[0]!=" " and tablero[0]==tablero[1] and tablero[1]==tablero[2]:
+     if tablero[0]!=" " and tablero[0]==tablero[1] and tablero[1]==tablero[2]:
         return True
-    if tablero[3]!=" " and tablero[3]==tablero[4] and tablero[4]==tablero[5]:
+     if tablero[3]!=" " and tablero[3]==tablero[4] and tablero[4]==tablero[5]:
         return True
-    if tablero[6]!=" " and tablero[6]==tablero[7] and tablero[7]==tablero[8]:
+     if tablero[6]!=" " and tablero[6]==tablero[7] and tablero[7]==tablero[8]:
         return True
-    return False
-    
+     return False
+def full(tablero):
+    return not " " in tablero
--- 58144 - Veliz, Celina/Clase 07/tateti.py
+++ 58144 - Veliz, Celina/Clase 07/tateti.py
@@ -1,2 +1,36 @@
-from tablero import draw_tablero, win ,validate
+from tablero import draw_tablero, win ,validate, full
 #draw_tablero([" "," "," "," "," "," "," "," "," "])
+tablero=[]
+for i in range (0,9):
+    tablero.append(" ")
+draw_tablero(tablero)
+while win(tablero)==False and full(tablero)==False:
+    print ("Es el turno de la X")
+    valido=False
+    while not valido:
+        print ("Ingrese una posicion valida del 1 al 9")
+        posicion=int(input())
+        valido=validate(tablero,posicion)
+        if not valido:
+            print("Error de posicion")
+    tablero[posicion -1]="X"
+    draw_tablero(tablero)
+    gano=win(tablero)
+    if gano:
+        print ("Gano X")
+    else:
+                print ("Es el turno de la 0")
+    valido=False
+    while not valido:
+        print ("Ingrese una posicion valida del 1 al 9")
+        posicion=int(input())
+        valido=validate(tablero,posicion)
+        if not valido:
+            print("Error de posicion")
+    tablero[posicion -1]="0"
+    draw_tablero(tablero)
+    gano=win(tablero)
+    if gano:
+       print ("Gano 0")
+if full(tablero):
+    print("Nadie gano")
